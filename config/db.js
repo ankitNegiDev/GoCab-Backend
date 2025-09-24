00b18d969216dfba61a9e1ba6eef1dc3dde1862a
ankitNegiDev <ankitnegi9104@gmail.com>
2025-09-24 12:06:54 +0530

    added the db configuration

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+let MONGO_DB = {
+  production: { url: process.env.MONGODB_PROD_URL, type: "Atlas" },
+  development: { url: process.env.MONGODB_DEV_URL, type: "Atlas" },
+};
+
+let environment = process.env.ENVIRONMENT;
+console.log("environment is : ",environment);
+
+mongoose
+  .connect(MONGO_DB[environment].url)
+  .then(() => {
+    console.log("Connected to Mongo DB", MONGO_DB[environment].type);
+  })
+  .catch(() => {
+    console.log("Failed to connect to MongoDB");
+  });
+
+module.exports = mongoose.connection;
